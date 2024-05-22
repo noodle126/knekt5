6e2b1fd93126042ab302d1d20a46f6661754f121
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-22 17:10:59 +0000

    fix main menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pages.buildTeam as buildteam
 def remove_side_bar():
     st.set_page_config(initial_sidebar_state="collapsed") 
     st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True,)
-
+    
 remove_side_bar()
 
 PAGES = {
@@ -17,13 +17,17 @@ PAGES = {
 
 def topHeader():
     st.header('Sonny Webster, 1220340000 rings', divider='rainbow')
-    col1, col2, col3 = st.columns([1,1,1])
+    col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
     
     with col1:
         st.page_link("pages/home.py", label="Home", icon=None)
     with col2:
-        st.page_link("pages/buildTeam.py", label="Build Team", icon=None)
+        st.page_link("pages/buildTeam.py", label="Teams", icon=None)
     with col3:
-        st.page_link("pages/buildTeam.py", label="Build Team", icon=None)
+        st.page_link("pages/buildTeam.py", label="Engage", icon=None)
+    with col4:
+        st.page_link("pages/buildTeam.py", label="Compete", icon=None)
+    with col5:
+        st.page_link("pages/buildTeam.py", label="Community", icon=None)
 
 topHeader()
