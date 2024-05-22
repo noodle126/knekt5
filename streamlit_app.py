39d821158e370e017ae3837d84beb21bece573fe
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-22 15:26:16 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,3 @@
 import streamlit as st
 
-
-st.sidebar.title("Kinekt")
-st.sidebar.caption("Engage")
-st.sidebar.caption("Earn")
-st.sidebar.page_link("build-team.py", label="Teams")
-st.sidebar.page_link("pages/user.py", label="Your profile")
-
-
 st.write("hello world")
