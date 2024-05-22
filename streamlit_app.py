c12aa28852ac9fe164fd81637a6db8e35b3e95b3
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-22 14:00:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+  
 st.write("hello world")
