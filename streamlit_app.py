cfe8db2473ba8970128a32d95b4adb25b2fcbef0
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-22 17:49:14 +0000

    fix unstable error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,6 @@
 import streamlit as st
-from streamlit.components.v1 import html
-import pages.home as home
-import pages.buildTeam as buildteam
+import components.all as ac
 
-# common functionality
-def remove_side_bar():
-    st.set_page_config(initial_sidebar_state="collapsed") 
-    st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True,)
-    
-remove_side_bar()
 
-PAGES = {
-    "Home": home,
-    "BuildTeams": buildteam
-}
-
-def topHeader():
-    st.header('Sonny Webster, 1220340000 rings', divider='rainbow')
-    col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
-    
-    with col1:
-        st.page_link("pages/home.py", label="Home", icon=None)
-    with col2:
-        st.page_link("pages/buildTeam.py", label="Teams", icon=None)
-    with col3:
-        st.page_link("pages/buildTeam.py", label="Engage", icon=None)
-    with col4:
-        st.page_link("pages/buildTeam.py", label="Compete", icon=None)
-    with col5:
-        st.page_link("pages/buildTeam.py", label="Community", icon=None)
-
-topHeader()
+ac.hideSideBar()
+ac.topHeader()
