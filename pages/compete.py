89c2b8ac13ee59344096f016dd8c81c180b67add
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-22 23:35:29 +0000

    fix

---
+++ pages/compete.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import components.all as ac
+from streamlit_card import card
+
+ac.topHeader()
+
+
+st.write("this is the page is about the quizs and ways to collect points")
---
+++ pages/engage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import components.all as ac
+from streamlit_card import card
+
+ac.topHeader()
+
+
+st.write("this is the page to leanr more about stuff")
