9d9e7e469607c0433ce45550048856be3e398e6c
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-23 13:29:38 +0000

    changes for teams page

--- pages/buildTeam.py
+++ pages/buildTeam.py
@@ -9,9 +9,9 @@ st.write("this is the page to build and manage teams")
 
 col1, col2, col3 = st.columns([1,1,1])    
 with col1:
-      hasClicked = card(title="I", text="Individual", image="https://raw.githubusercontent.com/noodle126/knekt5/images/individual.png", url="https://www.bbc.co.uk/",
-                        styles={"card": {"width": "150px", "height": "150px"}}
-                                )   
+      hasClicked = card(title="I", text="Individual", image="https://raw.githubusercontent.com/noodle126/knekt5/main/images/individual.png", url="https://www.bbc.co.uk/",
+                        styles={"card": {"width": "100%", "height": "100%"}}
+                                ) 
 with col2:
       hasClicked = card(title="IT", text="Individual Team", image="https://raw.githubusercontent.com/noodle126/knekt5/main/images/individualteam.png", url="https://www.bbc.co.uk/",
                         styles={"card": {"width": "100%", "height": "100%"}}
