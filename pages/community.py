5ff49086c38e5808cf3b1b836efd9345263078c8
tor22outfin <tor.outfin@effem.com>
2024-05-23 15:14:49 +0000

    debugging

--- pages/community.py
+++ pages/community.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import components.all as ac
+import os
 
 ac.topHeader()
 
 
 st.write("this is the page to about the posts")
+
+
+# Specify the directory you want to list
+directory = '/data/'
+
+# Get a list of files and directories in the specified directory
+files_and_directories = os.listdir(directory)
+
+# Print the list
+for item in files_and_directories:
+    print(item)
