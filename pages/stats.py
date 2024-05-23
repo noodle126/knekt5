3976dcca682b5769e4afe38573cc72a40ddf0693
kaoutar <84411806+Kaoutar-Nahli@users.noreply.github.com>
2024-05-23 16:56:41 +0100

    Update stats.py

--- pages/stats.py
+++ pages/stats.py
@@ -5,35 +5,35 @@ import components.all as ac
 ac.hideSideBar()
 ac.topHeader()
 # Load data (example)
-# data = {
-#     'Country': ['USA', 'China', 'Japan', 'Russia'],
-#     'Best Athlete': ['Michael Phelps', 'Liu Xiang', 'Kohei Uchimura', 'Alexander Karelin'],
-#     'Ranking': [1, 2, 3, 4],
-#     'Gold Medals in Sport': ['Swimming', 'Gymnastics', 'Gymnastics', 'Wrestling']
-# }
+data = {
+    'Country': ['USA', 'China', 'Japan', 'Russia'],
+    'Best Athlete': ['Michael Phelps', 'Liu Xiang', 'Kohei Uchimura', 'Alexander Karelin'],
+    'Ranking': [1, 2, 3, 4],
+    'Gold Medals in Sport': ['Swimming', 'Gymnastics', 'Gymnastics', 'Wrestling']
+}
 
-# df = pd.DataFrame(data)
-df= pd.read_csv('/data/in/tables/CLEAN_OLYMPIC_MEDALS.csv')
+df = pd.DataFrame(data)
+# df= pd.read_csv('/data/in/tables/CLEAN_OLYMPIC_MEDALS.csv')
 
 # Title of the app
-# st.title("Olympics Data Explorer")
-st.title(df.count())
+st.title("Olympics Data Explorer")
 
 
-# # Select favorite country
-# selected_country = st.selectbox("Select your favorite country", df['country_name'])
 
-# # Display relevant data for the selected country
-# if selected_country:
-#     country_data = df[df['Country'] == selected_country]
-#     best_athlete = country_data['Best Athlete'].values[0]
-#     ranking = country_data['Ranking'].values[0]
-#     sport_with_most_gold = country_data['Gold Medals in Sport'].values[0]
+# Select favorite country
+selected_country = st.selectbox("Select your favorite country", df['country_name'])
 
-#     st.header(f"Data for {selected_country}")
-#     st.write(f"Best Athlete: {best_athlete}")
-    # st.write(f"Ranking: {ranking}")
-    # st.write(f"Sport with Most Gold Medals: {sport_with_most_gold}")
+# Display relevant data for the selected country
+if selected_country:
+    country_data = df[df['Country'] == selected_country]
+    best_athlete = country_data['Best Athlete'].values[0]
+    ranking = country_data['Ranking'].values[0]
+    sport_with_most_gold = country_data['Gold Medals in Sport'].values[0]
+
+    st.header(f"Data for {selected_country}")
+    st.write(f"Best Athlete: {best_athlete}")
+    st.write(f"Ranking: {ranking}")
+    st.write(f"Sport with Most Gold Medals: {sport_with_most_gold}")
 
 # Footer
-st.write("Powered by Streamlit")
+# st.write("Powered by Streamlit")
