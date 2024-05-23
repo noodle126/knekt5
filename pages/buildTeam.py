603948470fc5a9734ccef9c8ca9d13d994879252
noodle126 <85793810+noodle126@users.noreply.github.com>
2024-05-23 12:36:15 +0000

    team images

--- pages/buildTeam.py
+++ pages/buildTeam.py
@@ -2,21 +2,23 @@ import streamlit as st
 import components.all as ac
 from streamlit_card import card
 
+ac.hideSideBar()
 ac.topHeader()
 
 st.write("this is the page to build and manage teams")
 
-col1, col2, col3 = st.columns([1,1,1])
-    
+col1, col2, col3 = st.columns([1,1,1])    
 with col1:
-      hasClicked = card(title="Join as an individual", text="Some description", image="/images/knekt_small_logo.png", url="https://www.bbc.co.uk/",
-                        styles={"card": {"width": "100%", "height": "100%"}}
+      hasClicked = card(title="I", text="Individual", image="https://raw.githubusercontent.com/noodle126/knekt5/images/individual.png", url="https://www.bbc.co.uk/",
+                        styles={"card": {"width": "150px", "height": "150px"}}
                                 )   
 with col2:
-      hasClicked = card(title="Join individual team", text="Some description", image="/images/knekt_small_logo.png", url="https://www.bbc.co.uk/",
+      hasClicked = card(title="IT", text="Individual Team", image="https://raw.githubusercontent.com/noodle126/knekt5/main/images/individualteam.png", url="https://www.bbc.co.uk/",
                         styles={"card": {"width": "100%", "height": "100%"}}
                                 )
 with col3:
-      hasClicked = card(title="Join as a global team", text="Some description", image="/images/knekt_small_logo.png", url="https://www.bbc.co.uk/",
+      hasClicked = card(title="GT", text="Global Team", image="https://raw.githubusercontent.com/noodle126/knekt5/main/images/knekt_logo.png", url="https://www.bbc.co.uk/",
                         styles={"card": {"width": "100%", "height": "100%"}}
                                 )
+      
+
